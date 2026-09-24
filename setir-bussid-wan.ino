a88727a9def9405398c4661edfe7bd059ff0d989
hermawandd <66829580+hermawandd@users.noreply.github.com>
2026-09-24 16:14:58 +0700

    Update setir-bussid-wan.ino

--- setir-bussid-wan.ino
+++ setir-bussid-wan.ino
@@ -2,19 +2,12 @@
 
 const int PIN_SETIR = 0; // GPIO 0 (Potensio Setir)
 
-// Parameter Smoothing khusus Potensio 100k
 float nilaiSmoothed = 2048.0; 
-float alpha = 0.08; // Semakin kecil (misal 0.05) semakin halus
+float alpha = 0.08; // Filter kehalusan
 
 void setup() {
-  // Inisialisasi Serial untuk ESP32-C3 USB CDC
-  Serial.begin(115200);
-  
-  // Tunggu koneksi USB siap (maksimal 2 detik)
-  unsigned long startWait = millis();
-  while (!Serial && (millis() - startWait < 2000)) {
-    delay(10);
-  }
+  // Paksa aktifkan USB CDC bawaan ESP32-C3
+  USBSerial.begin(115200);
 }
 
 void loop() {
@@ -23,11 +16,11 @@ void loop() {
   // Exponential Moving Average (EMA) Filter
   nilaiSmoothed = (alpha * raw) + ((1.0 - alpha) * nilaiSmoothed);
 
-  // Tampilkan ke Serial Terminal
-  Serial.print("Raw: ");
-  Serial.print(raw);
-  Serial.print(" | Halus: ");
-  Serial.println((int)nilaiSmoothed);
+  // Kirim data langsung via Hardware USB CDC
+  USBSerial.print("Raw: ");
+  USBSerial.print(raw);
+  USBSerial.print(" | Halus: ");
+  USBSerial.println((int)nilaiSmoothed);
 
   delay(20);
 }
