fc81d20e532d4fcc22f5a5aeb51b09a11b25f774
hermawandd <66829580+hermawandd@users.noreply.github.com>
2026-09-25 15:43:52 +0700

    Update setir-bussid-wan.ino

--- setir-bussid-wan.ino
+++ setir-bussid-wan.ino
@@ -1,21 +1,17 @@
 #include <BleKeyboard.h>
 
-BleKeyboard bleKeyboard("SETIR C3", "HF BUS", 100);
+BleKeyboard bleKeyboard("SETIR TESTING BARU GPT", "HF BUS", 100);
 
 void setup() {
   Serial.begin(115200);
 
   Serial.println("Starting BLE Keyboard...");
-
   bleKeyboard.begin();
 }
 
 void loop() {
-
   if (bleKeyboard.isConnected()) {
     Serial.println("BLUETOOTH CONNECTED!");
-
-    delay(3000);
 
     bleKeyboard.print("TEST C3");
 
