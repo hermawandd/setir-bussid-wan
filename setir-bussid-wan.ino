592e70a018597890002c3d0139b4eedf07950fb6
hermawandd <66829580+hermawandd@users.noreply.github.com>
2026-09-25 15:02:58 +0700

    Update setir-bussid-wan.ino

--- setir-bussid-wan.ino
+++ setir-bussid-wan.ino
@@ -1,10 +1,12 @@
+#define USE_NIMBLE
 #include <BleKeyboard.h>
+#include <NimBLEDevice.h>
 
-// Gunakan nama pendek "SETIR" agar paket BLE ringan & cepat di-respond MIUI
-BleKeyboard bleKeyboard("SETIR", "ESP32", 100);
+// Buat nama baru agar HP mengabaikan cache lama
+BleKeyboard bleKeyboard("SETIR C3 FIX", "ESP32", 100);
 
-const int GAS_PIN = 1;   // GPIO 1 (Pedal Gas)
-const int BRAKE_PIN = 2; // GPIO 2 (Pedal Rem)
+const int GAS_PIN = 1;   // GPIO 1
+const int BRAKE_PIN = 2; // GPIO 2
 
 void setup() {
   Serial.begin(115200);
@@ -12,7 +14,11 @@ void setup() {
   pinMode(GAS_PIN, INPUT_PULLUP);
   pinMode(BRAKE_PIN, INPUT_PULLUP);
 
-  // Inisialisasi Bluetooth BLE
+  // Inisialisasi NimBLE Manual Khusus ESP32-C3 untuk Mematikan Security MITM
+  NimBLEDevice::init("SETIR C3 FIX");
+  NimBLEDevice::setSecurityAuth(true, false, false); // Matikan MITM & PIN Security
+  NimBLEDevice::setSecurityIOCap(BLE_HS_IO_NO_INPUT_OUTPUT);
+
   bleKeyboard.begin();
 }
 
@@ -22,14 +28,14 @@ void loop() {
     return;
   }
 
-  // 1. TES PEDAL GAS (Tombol 'w')
+  // 1. PEDAL GAS ('w')
   if (digitalRead(GAS_PIN) == LOW) {
     bleKeyboard.press('w');
   } else {
     bleKeyboard.release('w');
   }
 
-  // 2. TES PEDAL REM (Tombol 's')
+  // 2. PEDAL REM ('s')
   if (digitalRead(BRAKE_PIN) == LOW) {
     bleKeyboard.press('s');
   } else {
