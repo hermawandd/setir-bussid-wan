2aa98fa07381109df2ceceb6fda1cf0d64cd1141
hermawandd <66829580+hermawandd@users.noreply.github.com>
2026-09-25 23:10:12 +0700

    tes sesok setir-bussid-wan.ino

--- setir-bussid-wan.ino
+++ setir-bussid-wan.ino
@@ -2,8 +2,7 @@
 #include <BleKeyboard.h>
 #include <NimBLEDevice.h>
 
-// Nama perangkat Bluetooth baru
-BleKeyboard bleKeyboard("SETIR C3 NIMBLE", "ESP32", 100);
+BleKeyboard bleKeyboard("SETIR C3 FIX", "ESP32", 100);
 
 const int GAS_PIN = 1;   // GPIO 1 (Pedal Gas)
 const int BRAKE_PIN = 2; // GPIO 2 (Pedal Rem)
@@ -14,11 +13,11 @@ void setup() {
   pinMode(GAS_PIN, INPUT_PULLUP);
   pinMode(BRAKE_PIN, INPUT_PULLUP);
 
-  // --- TRIK KHUSUS: Mengunci Konfigurasi NimBLE ala Gamepad ---
-  NimBLEDevice::init("SETIR C3 NIMBLE");
+  // Inisialisasi NimBLE dengan mode Enkripsi Bonding
+  NimBLEDevice::init("SETIR C3 FIX");
   
-  // Matikan semua proteksi bonding & enkripsi ketat Android
-  NimBLEDevice::setSecurityAuth(false, false, false);
+  // Setel keamanan sesuai rekomendasi perbaikan (Bonding tanpa PIN IO)
+  NimBLEDevice::setSecurityAuth(true, false, false, BLE_SM_PAIR_AUTHREQ_BOND);
   NimBLEDevice::setSecurityIOCap(BLE_HS_IO_NO_INPUT_OUTPUT);
 
   bleKeyboard.begin();
@@ -30,14 +29,14 @@ void loop() {
     return;
   }
 
-  // 1. PEDAL GAS ('w')
+  // Pedal Gas ('w')
   if (digitalRead(GAS_PIN) == LOW) {
     bleKeyboard.press('w');
   } else {
     bleKeyboard.release('w');
   }
 
-  // 2. PEDAL REM ('s')
+  // Pedal Rem ('s')
   if (digitalRead(BRAKE_PIN) == LOW) {
     bleKeyboard.press('s');
   } else {
